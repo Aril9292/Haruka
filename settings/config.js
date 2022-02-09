efb4ec1abea222dbc7418d75be7472c4f5da8756
Aril9292 <95112595+Aril9292@users.noreply.github.com>
2022-02-09 11:06:52 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -8,7 +8,7 @@ global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 global.ownername ="ARIL STORE"
 global.ownernumber = "62838957411144"
 global.botname = "Aril Botz-Md"
-global.thumbnail = fs.readFileSync("./settings/haruka.jpg") //sesuaikan dengan nama foto
+global.thumbnail = fs.readFileSync("./settings/20220120_161739-compress1.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.limit = {
 		free:20,
